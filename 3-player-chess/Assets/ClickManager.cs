550c91579e84e6c1fb118ba574fd4ad05a6c537f
Hugo Björs <hbjors@kth.se>
2022-04-28 21:12:19 +0200

    Add gradual movement

--- 3-player-chess/Assets/ClickManager.cs
+++ 3-player-chess/Assets/ClickManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ClickManager : MonoBehaviour
 {
-    public float speed = 5f;
+    Vector2 worldPoint;
     private bool nextClickWillMovePiece;
+    public float speed;
+    private bool moving;
     private GameObject piece;
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector2 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             RaycastHit2D hit = Physics2D.Raycast(worldPoint, Vector2.zero);
 
             if (hit.collider != null && hit.collider.CompareTag("piece"))
@@ -22,10 +24,21 @@ public class ClickManager : MonoBehaviour
             }
             else if (nextClickWillMovePiece)
             {
-                piece.transform.position = worldPoint;
+                moving = true;
                 nextClickWillMovePiece = false;
+                //Below is the instant movement version
+                // piece.transform.position = worldPoint;
+                // float step = speed*Time.deltaTime;
             }
         }
-
+        if (moving && !piece.transform.position.Equals(worldPoint))
+        {
+            float step = speed * Time.deltaTime;
+            piece.transform.position = Vector2.MoveTowards(piece.transform.position, worldPoint, step);
+        }
+        else
+        {
+            moving = false;
+        }
     }
 }
